e388c65de226301e54f74db3045d393df6edcd86
Selcuk Cihan <selcukcihan@gmail.com>
2019-11-27 11:24:10 +0300

    Fix the blog post link

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="margin-auto-block">
             <p>This is a toy project. We only store location information as displayed on Twitter, of the people you follow.</p>
             <p>You can see <a href="/heatmap">a world map</a> displaying locations of the people you follow.</p>
-            <p>There's a blog post about how the project was built on <a href="https://blog.selcukcihan.com">SITE</a>.</p>
+            <p>There's a <a href="https://blog.selcukcihan.com">blog post</a> about how the project was built, in Turkish.</p>
             <p>Both <a href="https://github.com/selcukcihan/twheat-map">frontend</a>
                 &nbsp;and <a href="https://github.com/selcukcihan/twheat-map-backend">backend</a>
                 &nbsp;repositories are both public.
