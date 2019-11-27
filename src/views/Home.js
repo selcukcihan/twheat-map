4065a9a0820ecb4857ce2ed5b3b4ce76ad33d64c
Selcuk Cihan <selcukcihan@gmail.com>
2019-11-27 22:06:49 +0300

    Update blog post link

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="margin-auto-block">
             <p><a href="/heatmap">Click here</a> to display locations of the people you follow.</p>
             <p>This is a toy project. We only store location information as displayed on Twitter, of the people you follow.</p>
-            <p>There's a <a href="https://blog.selcukcihan.com">blog post</a> about how the project was built, in Turkish.</p>
+            <p>There's a <a href="https://blog.selcukcihan.com/web-development/twheat-map/">blog post</a> about how the project was built, in Turkish.</p>
             <p>Both <a href="https://github.com/selcukcihan/twheat-map">frontend</a>
                 &nbsp;and <a href="https://github.com/selcukcihan/twheat-map-backend">backend</a>
                 &nbsp;repositories are public.
