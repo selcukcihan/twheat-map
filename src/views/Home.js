f8d1a4bab1fbb4be513d3f9b1ed22687259d5d5b
Selcuk Cihan <selcukcihan@gmail.com>
2019-11-27 21:54:45 +0300

    Change wording on home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -3,12 +3,12 @@ import React, { Fragment } from "react";
 const Home = () => {
     return (
         <div className="margin-auto-block">
+            <p><a href="/heatmap">Click here</a> to display locations of the people you follow.</p>
             <p>This is a toy project. We only store location information as displayed on Twitter, of the people you follow.</p>
-            <p>You can see <a href="/heatmap">a world map</a> displaying locations of the people you follow.</p>
             <p>There's a <a href="https://blog.selcukcihan.com">blog post</a> about how the project was built, in Turkish.</p>
             <p>Both <a href="https://github.com/selcukcihan/twheat-map">frontend</a>
                 &nbsp;and <a href="https://github.com/selcukcihan/twheat-map-backend">backend</a>
-                &nbsp;repositories are both public.
+                &nbsp;repositories are public.
             </p>
         </div>
     );
