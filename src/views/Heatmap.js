34fbd6287a9ee35dc88aba15bb5223c05a6be674
Selcuk Cihan <selcukcihan@gmail.com>
2019-11-27 12:46:34 +0300

    Display map even without any data

--- src/views/Heatmap.js
+++ src/views/Heatmap.js
@@ -45,41 +45,41 @@ const Heatmap = () => {
         }
     });
 
-    if (apiMessage === null) {
-        return <Loading className="spinner-2" />;
-    }
-
     return (
-        <ComposableMap className="margin-auto-block">
-            <Geographies geography={topoJson}>
-                {({ geographies }) =>
-                    geographies
-                        .map(geo => (
-                            <Geography
-                                key={geo.rsmKey}
-                                geography={geo}
-                                fill="#EAEAEC"
-                                stroke="#D6D6DA"
-                            />
-                        ))
-                }
-            </Geographies>
-            {apiMessage.map((coordinates, index) => (
-                <Marker key={index} coordinates={[coordinates.lng, coordinates.lat]}>
-                    <g
-                        fill="none"
-                        stroke="#FF5533"
-                        strokeWidth="1"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        transform={getRandomizedOffset()}
-                    >
-                        <circle cx="12" cy="10" r="3" />
-                        <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-                    </g>
-                </Marker>
-            ))}
-        </ComposableMap>
+        <>
+            {!apiMessage && (
+            <Loading className="spinner-2" />)}
+            <ComposableMap className="margin-auto-block">
+                <Geographies geography={topoJson}>
+                    {({ geographies }) =>
+                        geographies
+                            .map(geo => (
+                                <Geography
+                                    key={geo.rsmKey}
+                                    geography={geo}
+                                    fill="#EAEAEC"
+                                    stroke="#D6D6DA"
+                                />
+                            ))
+                    }
+                </Geographies>
+                {apiMessage && apiMessage.map((coordinates, index) => (
+                    <Marker key={index} coordinates={[coordinates.lng, coordinates.lat]}>
+                        <g
+                            fill="none"
+                            stroke="#FF5533"
+                            strokeWidth="1"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            transform={getRandomizedOffset()}
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
+                    </Marker>
+                ))}
+            </ComposableMap>
+        </>
     );
 };
 
